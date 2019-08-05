cb8701fcf423d5c7b3bc1c90e80fc0c3463d0b2b
fqingyu <2309686@qq.com>
2019-08-05 16:46:05 -0500

    INIT

---
+++ precache-manifest.d5ba2e4f2acc99d131f133e621ab21f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28033e041c0e5eb61a95170adf46b665",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ccb30bc6b6a69cd9632",
+    "url": "./static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "3c28efaed7d10a55fd37",
+    "url": "./static/css/main.20490f33.chunk.css"
+  },
+  {
+    "revision": "6ccb30bc6b6a69cd9632",
+    "url": "./static/js/2.f9b9d2e1.chunk.js"
+  },
+  {
+    "revision": "3c28efaed7d10a55fd37",
+    "url": "./static/js/main.ff7bad78.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
